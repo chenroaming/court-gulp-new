a2874f334d1c93591c715b7e0e587e25e4fdf924
cxm <839047108@qq.com>
2019-08-28 17:48:06 +0800

    增加新闻页的首航缩进和换行符

--- src/js/news.js
+++ src/js/news.js
@@ -10,7 +10,8 @@ const res = ajaxGet('/api/main/homeNews/getHomeNewsInfo.jhtml',{newsId:newsId});
 console.log(res);
 $('#newsTitle').text(res.data.newsTitle);
 $('#newsPic').attr('src',res.data.imgUrl);
-$('.content').html(res.data.content);
+const newsContent = res.data.content.replace(/(\r\n|\n|\r)/gm, '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');//换行符正则替换
+$('.content').html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+newsContent);
 const time = new Date(res.data.createDate);
 $('#newsTime').text(time.getFullYear()+'年'+(time.getMonth()+1)+'月'+time.getDate()+'日'+'  '+time.getHours()+':'+time.getMinutes());
 //ajax的小封装
