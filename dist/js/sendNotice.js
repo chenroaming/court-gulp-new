a0242ff4dbf91cbef7ff0feec71d22d0e1ea5968
cxm <839047108@qq.com>
2019-09-20 10:24:50 +0800

    修复送达公告链接错误的BUG

--- dist/js/sendNotice.js
+++ dist/js/sendNotice.js
@@ -14,28 +14,31 @@ $(document).ready(function () {
     // })
 
     const notice = ajaxGet('/api/main/homeNews/getSendNoticeList.jhtml',{pageNum:1,pageSize:10});
-    console.log(notice);
     const totalPage = Math.ceil(notice.total / 10);
-    for (const item of notice.date){
-        const div = ('<div class="content"><div><img src="../images/laba.png" alt=""></div><a href="'+item.address+'" target="_blank">致'+item.litigant_name+'公告</a><p>'+item.holdTime+'</p></div>');
-        $('#list').append(div);
-    }
+    transform(notice.date);
     $("#Pagination").paging({
       nowPage: 1, // 当前页码,默认为1
       pageNum: totalPage, // 总页码
       buttonNum: 7, //要展示的页码数量，默认为7，若小于5则为5
       callback: function(num) { //回调函数,num为当前页码
         const notice = ajaxGet('/api/main/homeNews/getSendNoticeList.jhtml',{pageNum:num,pageSize:10});
-        $('#list').empty();
-        for (const item of notice.date){
-          const div = ('<div class="content"><div><img src="../images/laba.png" alt=""></div><a href="'+item.address+'" target="_blank">致'+item.litigant_name+'公告</a><p>'+item.holdTime+'</p></div>');
-          $('#list').append(div);
-      }
-        console.log(num);
+        transform(notice.date);
       }
     });
 })
 
+function transform(urlList){
+  $('#list').empty();
+  for (const item of urlList){
+    let div = '';
+    if(item.address.split('https://').length <= 2){
+      div = ('<div class="content"><div><img src="../images/laba.png" alt=""></div><a href="'+item.address+'" target="_blank">致'+item.litigant_name+'公告</a><p>'+item.holdTime+'</p></div>');
+    }else{
+      div = ('<div class="content"><div><img src="../images/laba.png" alt=""></div><a href="https://hlcourt.obs.cn-south-1.myhuaweicloud.com:443/'+ajaxGet('/api/main/getLocalPath.jhtml',{path:item.address}).path+'" target="_blank">致'+item.litigant_name+'公告</a><p>'+item.holdTime+'</p></div>');
+    }
+    $('#list').append(div);
+  }
+}
 
 //ajax的小封装
 function ajaxGet(url,data =''){
