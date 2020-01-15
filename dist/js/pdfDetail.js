027b7d1af0e1285a3788e2903d057529f4bd8e45
cxm <839047108@qq.com>
2020-01-15 09:52:18 +0800

    送达公告页修改及部分样式修复

--- dist/js/pdfDetail.js
+++ dist/js/pdfDetail.js
@@ -1,10 +1,21 @@
 
-let pdfUrl = window.location.href.split('url=')[1];
-if(!pdfUrl){
-    window.location.href = 'index.html';
+const fileType = window.location.href.split('.')[window.location.href.split('.').length-1];
+if(fileType == 'pdf'){
+    let pdfUrl = window.location.href.split('url=')[1];
+    if(!pdfUrl){
+        window.location.href = 'index.html';
+    }
+    if(pdfUrl.indexOf('dqfile.hlcourt.gov.cn') != -1){
+        pdfUrl = pdfUrl.replace('http://dqfile.hlcourt.gov.cn','https://hlcourt.obs.cn-south-1.myhuaweicloud.com:443');
+    }
+    PDFObject.embed(pdfUrl, "#content");
+}else{
+    let docUrl = window.location.href.split('url=')[1];
+    if(docUrl.indexOf('court1.ptnetwork001.com') != -1){
+        $('#wordSrc').attr('src','https://view.officeapps.live.com/op/view.aspx?src=' + docUrl);
+    }else{
+        docUrl = 'https://court1.ptnetwork001.com/' + docUrl;
+        $('#wordSrc').attr('src','https://view.officeapps.live.com/op/view.aspx?src=' + docUrl);
+    }
+    $('#wordSrc').show();
 }
-if(pdfUrl.indexOf('dqfile.hlcourt.gov.cn') != -1){
-    pdfUrl = pdfUrl.replace('http://dqfile.hlcourt.gov.cn','https://hlcourt.obs.cn-south-1.myhuaweicloud.com:443');
-}
-console.log(pdfUrl);
-PDFObject.embed(pdfUrl, "#content");
